ec0860c15cafafcd2e9d2882a03d4546e30f01e4
Rodrigo Llull <gabriel_llull15@hotmail.com>
2022-11-23 15:40:53 -0300

    Pequeñas modificaciones de estilo a la seccion de mi perfil y lista de productos

--- js/my-profile.js
+++ js/my-profile.js
@@ -1,86 +1,80 @@
 // Variables
-let formulario = document.querySelector('#formulario')
-let imagenPerfil = document.querySelector('#imagenPerfil')
-let nombre = document.querySelector('#nombre')
-let segundoNombre = document.querySelector('#segundoNombre')
-let apellido = document.querySelector('#apellido')
-let segundoApellido = document.querySelector('#segundoApellido')
-let email = document.querySelector('#email')
-let agregarImagenPerfil = document.querySelector('#agregarImagenPerfil')
-let telefonoContacto = document.querySelector('#telefonoContacto')
-let guardarCambios = document.querySelector('#guardarCambios')
-
+let formulario = document.querySelector('#formulario');
+let imagenPerfil = document.querySelector('#imagenPerfil');
+let nombre = document.querySelector('#nombre');
+let segundoNombre = document.querySelector('#segundoNombre');
+let apellido = document.querySelector('#apellido');
+let segundoApellido = document.querySelector('#segundoApellido');
+let email = document.querySelector('#email');
+let agregarImagenPerfil = document.querySelector('#agregarImagenPerfil');
+let telefonoContacto = document.querySelector('#telefonoContacto');
+let guardarCambios = document.querySelector('#guardarCambios');
 
 // Funciones
 function setearNombreYApellido() {
-    if (nombre.value !== ''){
-        localStorage.setItem('nombreUsuario', nombre.value)
-    }
-    if (apellido.value !== ''){
-        localStorage.setItem('apellidoUsuario', apellido.value)
-    }
-}
+    if (nombre.value !== '') {
+        localStorage.setItem('nombreUsuario', nombre.value);
+    };
+    if (apellido.value !== '') {
+        localStorage.setItem('apellidoUsuario', apellido.value);
+    };
+};
 
 function validarFormulario(e) {
-    if (nombre.value == '') {  
-        e.preventDefault()  
+    if (nombre.value == '') {
+        e.preventDefault();
         e.stopPropagation();
-        nombre.classList.add('is-invalid')
+        nombre.classList.add('is-invalid');
         console.log(agregarImagenPerfil.flies);
-    }
+    };
 
     if (apellido.value == '') {
-        e.preventDefault()  
+        e.preventDefault();
         e.stopPropagation();
-        apellido.classList.add('is-invalid')
-    }
+        apellido.classList.add('is-invalid');
+    };
     if (email.value == '') {
-        e.preventDefault()  
+        e.preventDefault();
         e.stopPropagation();
-        email.classList.add('is-invalid')
-    }
-}
+        email.classList.add('is-invalid');
+    };
+};
 
+// Al cargar el DOM cargar los datos de los inputs correspondientes, habiendolos seteado previamente
 function cargarDatos() {
     if (localStorage.getItem('usuario')) {
-        let usuario = localStorage.getItem('usuario')
-        email.value = usuario
-    } 
+        let usuario = localStorage.getItem('usuario');
+        email.value = usuario;
+    };
     if (localStorage.getItem('nombreUsuario')) {
-        let nombreUsuario = localStorage.getItem('nombreUsuario')
-        nombre.value = nombreUsuario
-        
-    }
+        let nombreUsuario = localStorage.getItem('nombreUsuario');
+        nombre.value = nombreUsuario;
+    };
     if (localStorage.getItem('apellidoUsuario')) {
-        let apellidoUsuario = localStorage.getItem('apellidoUsuario')
-        apellido.value = apellidoUsuario 
-    } 
+        let apellidoUsuario = localStorage.getItem('apellidoUsuario');
+        apellido.value = apellidoUsuario;
+    };
     if (localStorage.getItem('imagenPerfil')) {
-        let imagenPerfilLS = localStorage.getItem('imagenPerfil')
-        imagenPerfil
-        imagenPerfil.setAttribute('src', imagenPerfilLS)
-    }
-}
+        let imagenPerfilLS = localStorage.getItem('imagenPerfil');
+        imagenPerfil.setAttribute('src', imagenPerfilLS);
+    };
+};
 
-
-document.addEventListener('DOMContentLoaded', () => { 
-    cargarDatos()
-
-    
-})
-
+// Eventos
+document.addEventListener('DOMContentLoaded', () => {
+    cargarDatos();
+});
 
 formulario.addEventListener('submit', (e) => {
-    setearNombreYApellido()
-    validarFormulario(e)
-})
+    setearNombreYApellido();
+    validarFormulario(e);
+});
 
 agregarImagenPerfil.addEventListener('change', () => {
     const lector = new FileReader();
-    lector.addEventListener('load', ()=>{
-        localStorage.setItem('imagenPerfil', lector.result)
-    })
+    lector.addEventListener('load', () => {
+        localStorage.setItem('imagenPerfil', lector.result);
+    });
     lector.readAsDataURL(agregarImagenPerfil.files[0]);
-    
-})
 
+});
